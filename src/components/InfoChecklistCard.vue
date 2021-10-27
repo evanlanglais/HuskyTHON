8cda5f550592292573d93501de3f30a193708428
Evan Langlais <evanlanglais@gmail.com>
2021-10-26 17:53:50 -0700

    feat(info): spice up info cards

--- src/components/InfoChecklistCard.vue
+++ src/components/InfoChecklistCard.vue
@@ -1,11 +1,20 @@
 <template>
-  <ion-card @click="setOpen(true)" color="secondary">
+  <ion-card @click="setOpen(true)">
     <ion-card-header>
       <ion-card-title>Event Item Checklist</ion-card-title>
     </ion-card-header>
 
     <ion-card-content>
-      Tap to view the list of all the essential items you should <strong>and shouldn't</strong> bring for the big event!
+      <ion-grid>
+        <ion-row>
+          <ion-col size="4">
+            <ion-icon :icon="bagCheckOutline" class="card-icon ion-no-margin ion-no-padding"></ion-icon>
+          </ion-col>
+          <ion-col size="8">
+            <ion-label>Tap to view the list of all the essential items you should <strong>and shouldn't</strong> bring for the big event!</ion-label>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-card-content>
 
     <ion-modal
@@ -21,21 +30,30 @@
 </template>
 
 <script lang="ts">
-import {IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonModal} from "@ionic/vue";
+import {IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonModal, IonIcon, IonGrid, IonCol, IonRow, IonLabel} from "@ionic/vue";
 import { ref, defineComponent } from 'vue';
 import InfoChecklistModal from "@/components/InfoChecklistModal.vue";
+import {bagCheckOutline} from "ionicons/icons";
 
 export default defineComponent({
   name: "InfoChecklistCard",
-  components: {InfoChecklistModal, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonModal},
+  components: {InfoChecklistModal, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonModal, IonIcon, IonGrid, IonCol, IonRow, IonLabel},
   setup() {
     const isOpenRef = ref(false);
     const setOpen = (state: boolean) => isOpenRef.value = state;
-    return { isOpenRef, setOpen }
+    return {
+      isOpenRef,
+      setOpen,
+      bagCheckOutline
+    }
   }
 });
 </script>
 
 <style scoped>
-
+.card-icon {
+  width: 100%;
+  font-size: 64px;
+  color: #1B1464;
+}
 </style>
--- src/components/InfoContactCard.vue
+++ src/components/InfoContactCard.vue
@@ -1,25 +1,36 @@
 <template>
-  <ion-card color="secondary">
+  <ion-card>
     <ion-card-header>
       <ion-card-subtitle>Contact Information</ion-card-subtitle>
     </ion-card-header>
     <ion-card-content>
-      <ion-item lines="none" color="secondary" class="ion-text-center">
-        <ion-label>exec@huskython.org</ion-label>
-      </ion-item>
-      <ion-item lines="none" color="secondary" class="ion-text-center">
-        <ion-label>dancer.relations@huskython.org</ion-label>
-      </ion-item>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="8">
+            <ion-label>exec@huskython.org</ion-label><br/>
+            <ion-label>dancer.relations@huskython.org</ion-label>
+          </ion-col>
+          <ion-col size="4">
+            <ion-icon :icon="mailOutline" class="card-icon ion-no-margin ion-no-padding"></ion-icon>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-card-content>
   </ion-card>
 </template>
 
 <script>
-import {IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonItem, IonLabel} from "@ionic/vue";
+import {IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonLabel, IonGrid, IonRow, IonCol, IonIcon} from "@ionic/vue";
+import {mailOutline} from "ionicons/icons";
 
 export default {
   name: "InfoContactCard",
-  components: {IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonItem, IonLabel}
+  components: {IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonLabel, IonGrid, IonRow, IonCol, IonIcon},
+  setup() {
+    return {
+      mailOutline
+    }
+  }
 }
 </script>
 
@@ -27,5 +38,10 @@ export default {
  ion-item {
    --padding-start: 0;
    --padding-end: 0;
+ }
+ .card-icon {
+   width: 100%;
+   font-size: 64px;
+   color: #1B1464;
  }
 </style>
